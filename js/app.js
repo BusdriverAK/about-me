83bc205d4301ef5fa9d3e0c37e0231403baf9330
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2018-04-03 16:55:23 -0700

    Just finished my first question

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var ownDogs = prompt('Do I own German Shepherds?')
+if(ownDogs === 'yes') {
+    alert('That is correct!');
+} else if(ownDogs === 'no'){
+    alert('Hahaha! Nope nice try though');
+} else {
+    alert('Please answer with yes or no');
+}
+
