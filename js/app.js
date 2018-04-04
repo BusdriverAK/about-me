8e83e11c275b238c7fea20234ae11b938dc44163
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2018-04-03 18:14:52 -0700

    The football team question finally got it to work

--- js/app.js
+++ js/app.js
@@ -1,18 +1,28 @@
 'use strict';
 
 var ownDogs = prompt('Do I own German Shepherds?')
-if(ownDogs === 'yes') {
+if(ownDogs === 'Yes') {
     alert('That is correct!');
-} else if(ownDogs === 'no'){
+} else if(ownDogs === 'No'){
     alert('Hahaha! Nope nice try though');
 } else {
     alert('Please answer with yes or no');
 }
 var favColor = prompt('Is my favorite color black or green?')
-if(favColor ==='green') {
+if(favColor ==='Green') {
     alert('Wow you must know me very well then!');
- } else if(favColor === 'black'){
+ } else if(favColor === 'Black'){
         alert('Are you crazy why are you even taking this test?!?!');
     } else{
         alert('Please answer with green or black!').toLowerCase();
 }
+var favTeam = prompt('Am I a Seahawks fan or a Raiders fan?')
+if(favTeam === 'Raiders') {
+   alert('That is right I bleed black and silver baby!');
+} else if(favTeam === 'Seahawks'){
+    alert('Nope sorry wrong answer was not rasied a Seahawks fan!');
+ }else{
+     alert('Please answer with Raiders or Seahawks');
+
+    }
+
